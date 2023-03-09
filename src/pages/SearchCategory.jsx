85cb52567786ac41ba3c039e988df85b8bda9903
allbutgold <contact@leokieffer.com>
2023-03-09 17:45:09 +0100

    write logic for catCard component add AreaSlider to SearchArea page

--- src/pages/SearchCategory.jsx
+++ src/pages/SearchCategory.jsx
@@ -1,10 +1,34 @@
+// import library
 import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+
+// import components
+import CatCards from '../components/catCards/CatCards.jsx'
 
 const SearchCategory = () => {
-
     const { cat } = useParams()
+    const [catResult, setCatResult] = useState([])
+    
+
+    useEffect(() => {
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${cat}`)
+            .then(res => res.json())
+            .then(data => {
+                setCatResult(data.meals)
+                // console.log(catResult)
+            })
+    }, [catResult, cat])
+
     return (
-        <h1>{cat}</h1>
+        <section>
+            {
+                catResult && 
+                    catResult.map(cat => {
+                        return( <CatCards key={cat.idMeal} cat={cat} />)
+})}
+            
+        </section>
+        
     );
 }
 

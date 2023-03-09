eb665bcab69230da0c1fdb4e3a20412c2e58342e
domieee <gartzdominik@gmail.com>
2023-03-09 11:50:36 +0100

    created Link to '/onboarding'

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -1,6 +1,31 @@
+import { Link } from 'react-router-dom'
+
+import LogoSymbol from '../img/logoSymbol.png'
+import LogoTypo from '../img/logoTypo.png'
+
+/* 
+TODOS:
+    TODO: import and add vector graphics from src/img
+ */
 const SplashScreen = () => {
-    return ( 
-        <h1>This is the SplashScreen page</h1>
+    return (
+        <Link to='/onboarding'>
+            <section
+                style={{
+                    backgroundColor: '#70B9BE'
+                }}
+                className="splashScreen" >
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    maxWidth: '300px'
+                }}>
+                    <img src={LogoSymbol} alt="" />
+                    <img src={LogoTypo} alt="" />
+                </div>
+
+            </section>
+        </Link >
     );
 }
 

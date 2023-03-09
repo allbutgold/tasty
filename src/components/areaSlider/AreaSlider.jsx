77f35e736068008e10a29516c5bb21dc724979c7
nizza1 <108374160+nizza1@users.noreply.github.com>
2023-03-09 15:08:01 +0100

    nnn

--- src/components/areaSlider/AreaSlider.jsx
+++ src/components/areaSlider/AreaSlider.jsx
@@ -5,26 +5,38 @@ import { Link } from 'react-router-dom';
 const AreaSlider = () => {
 
     const [area, setArea] = useState([]);
-
+    const [toggle, setToggle] = useState(false);
     useEffect(() => {
         fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
         .then((response) => response.json())
         .then((data) => setArea(data.meals))
     }, [])
+console.log(toggle)
+
     return (
         <section>
             <div>
                 <h2>Areas</h2>
-                <Link to='/SearchArea'>See All</Link>
+                <button onClick={()=> setToggle(!toggle)}>See All</button>
             </div>
             {area.map((item, i) => {
+              
                 return (
-                    <button>{item.strArea}</button>
+                    toggle ? <button>{item.strArea}</button> :null
                 )
-            })}
+                    }
+    
+                )}
+                
+                {toggle? null : <div>
+                    <button>American</button>
+                    <button>British</button>
+                    <button>Canadian</button>
+                    </div>}
         </section>
         
     );
+
 }
 
 export default AreaSlider

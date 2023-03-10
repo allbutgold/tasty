08d230414f2ef8babc8a9f1a2c868e7ca303ca0b
domieee <gartzdominik@gmail.com>
2023-03-10 14:29:06 +0100

    added icons to navigation

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,17 +1,41 @@
 //import library
 import { Link } from 'react-router-dom'
 
+import iconHome from '../../img/icons/iconHome.svg'
+import iconSearch from '../../img/icons/iconSearch.svg'
+import iconHeart from '../../img/icons/iconHeart.svg'
+import iconUser from '../../img/icons/iconUser.svg'
+
 
 const Navigation = () => {
+
+
+
     return (
         <section className='navigation'>
             <nav>
-                <Link to='/'>SplashScreen</Link>
-                <Link to='/onboarding'>Onboarding</Link>
-                <Link to='/home'>Home</Link>
-                <Link to='/searchcategory'>searchcategory</Link>
-                <Link to='/searchresult'>searchresult</Link>
-                <Link to='/detailspage'>detailspage</Link>
+                <Link to='/home'>
+                    <img src={iconHome} alt="" />
+                </Link>
+                <Link to='/searchResult'>
+                    <img src={iconSearch} alt="" />
+                </Link>
+                <Link
+                    to='/'
+                    style={{
+                        pointerEvents: 'none',
+                        opacity: '60%'
+                    }}>
+                    <img src={iconHeart} alt="" />
+                </Link>
+                <Link
+                    to='/searchcategory'
+                    style={{
+                        pointerEvents: 'none',
+                        opacity: '60%'
+                    }}>
+                    <img src={iconUser} alt="" />
+                </Link>
             </nav>
         </section>
     );

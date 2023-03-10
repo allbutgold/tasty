2f540b35d8fa778363e331797f684de10ef924b6
domieee <gartzdominik@gmail.com>
2023-03-10 11:33:14 +0100

    toggle state on catslider component

--- src/components/catSlider/CatSlider.jsx
+++ src/components/catSlider/CatSlider.jsx
@@ -3,31 +3,64 @@ import { Link } from 'react-router-dom'
 
 /* 
 TODOS:
-    TODO: remove link 'see all' and add button instead
+    
 */
 
 const CatSlider = () => {
 
     const [cat, setCat] = useState([])
+    const [toggle, setToggle] = useState(true);
 
     useEffect(() => {
-        fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
+        fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
             .then(res => res.json())
-            .then(data => setCat(data.meals))
+            .then(data => setCat(data.categories))
     }, [])
+
+    console.log(cat);
 
     return (
         <section className="catSlider">
             <div>
                 <h2>Categories</h2>
-                <Link to='/#'>See All</Link>
+                <button onClick={() => setToggle(!toggle)}>See All</button>
             </div>
-            {cat.map((category) => {
-                return (
-                    <Link to={`/searchcategory/${category.strCategory}`}>{category.strCategory}</Link>
-                )
-            })}
 
+            <article style={{ display: 'flex' }}>
+                {cat.map((category) => {
+                    return (
+                        toggle ?
+                            <Link to={`/searchcategory/${category.strCategory}`}>
+                                <div>
+                                    <img src={category.strCategoryThumb} alt="" />
+                                    {category.strCategory}
+                                </div>
+                            </Link> : null)
+                })}
+
+                {toggle ? null :
+                    <>
+                        <Link to={'/searchcategory/Beef'}>
+                            <div>
+                                <img src={cat[0].strCategoryThumb} alt="" />
+                                Beef
+                            </div>
+                        </Link>
+                        <Link to={'/searchcategory/Chicken'}>
+                            <div>
+                                <img src={cat[1].strCategoryThumb} alt="" />
+                                Chicken
+                            </div>
+                        </Link>
+                        <Link to={'/searchcategory/Dessert'}>
+                            <div>
+                                <img src={cat[2].strCategoryThumb} alt="" />
+                                Dessert
+                            </div>
+                        </Link>
+                    </>
+                }
+            </article>
         </section >
     );
 }

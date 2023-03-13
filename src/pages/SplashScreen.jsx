b0c404148755e4fe9474838d5cfef5f1348e514c
allbutgold <contact@leokieffer.com>
2023-03-13 10:20:43 +0100

    add roboto font

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom'
 import { gsap } from "gsap";
 
 // import images
-import logoTypo from '../img/logoTypo.png'
+import logo from '../img/Logo.svg'
 
 // import scss
 import styles from './splashScreen.module.scss'
 
 const SplashScreen = () => {
-    gsap.to(".tasty", { x: 200 })
+    
     return (
         <Link to='/onboarding' className={styles.splashScreen}>
             <section>
                 <h1>TASTY</h1>
-                <img src={logoTypo} alt="" />
+                <img src={logo} alt="" />
 
 
             </section>
